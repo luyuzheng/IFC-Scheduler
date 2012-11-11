be4fd6b73c8e474f38a73a1902ec7a6d378b355c
Claire Cipriani <cec247@cornell.edu>
2012-11-11 17:05:36 -0500

    changed room to SchedulePractitionerDto

--- IFC_Source_Code/src/gui/main/RoomSubpanel.java
+++ IFC_Source_Code/src/gui/main/RoomSubpanel.java
@@ -12,19 +12,20 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
-import data.Appointment;
+import backend.DataTransferObjects.AppointmentDto;
+import backend.DataTransferObjects.SchedulePractitionerDto;
+
 import gui.Constants;
-import data.Room;
 
 public class RoomSubpanel extends JPanel{
-	private Room room;
+	private SchedulePractitionerDto room;
 	private DayPanel dp;
 	
-	public RoomSubpanel(Room room, DayPanel dp) {
+	public RoomSubpanel(SchedulePractitionerDto room, DayPanel dp) {
 		this.dp = dp;
 		this.room = room;
-		if (room.hasPrac()) 
-			setAppointments();
+		//if (room.hasPrac()) 
+			//setAppointments();
 		setBorder(BorderFactory.createMatteBorder(0, 1, 1, 0, Color.BLACK));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBackground(Color.LIGHT_GRAY);
@@ -36,16 +37,16 @@ public class RoomSubpanel extends JPanel{
 	public void setAppointments() {
 		removeAll();
 		repaint();
-		for (Appointment a : room.getAppointments()) {
+		for (AppointmentDto a : room.getAppointments()) {
 			AppointmentBlock app = new AppointmentBlock(a,dp);
 			add(app);
 		}
-		if (room.getLeftover() > 0) {
+		/**if (room.getLeftover() > 0) {
 			JPanel leftover = new JPanel();
 			leftover.setPreferredSize(new Dimension(0,room.getLeftover()*Constants.PIXELS_PER_MINUTE));
 			leftover.setBackground(Color.LIGHT_GRAY);
 			add(leftover);
-		}
+		}**/
 	}
 	
 	public DayPanel getDayPanel() {
