c5a3b35e4d7b4bfbe3df7bf367e6bbc3d7420d22
Claire Cipriani <cec247@cornell.edu>
2012-11-11 18:17:25 -0500

    changed something

--- IFC_Source_Code/src/gui/main/DayPanel.java
+++ IFC_Source_Code/src/gui/main/DayPanel.java
@@ -118,7 +118,7 @@ public class DayPanel extends JPanel {
 		ab = a;
 		patientButton.setEnabled(b);
 		if (b) {
-			if (ab.getAppointment().isFilled())
+			if (ab.getAppointment().getPatientID() != null)
 				patientButton.setAction(removePatAction);
 			else
 				patientButton.setAction(addPatAction);
