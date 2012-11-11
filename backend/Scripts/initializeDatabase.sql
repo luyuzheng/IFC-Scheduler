157f368644ce3a04ccf7b9efdf483445fcf26bdb
Kenny <kenny@kenny-ALTO.(none)>
2012-11-11 13:41:04 -0500

    changed database slightly

--- backend/Scripts/initializeDatabase.sql
+++ backend/Scripts/initializeDatabase.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS ifc_db;
 CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'test623';
+USE ifc_db;
 GRANT ALL ON testdb.* TO 'testuser'@'localhost';
 CREATE TABLE IF NOT EXISTS Patient
 (
@@ -60,7 +61,8 @@ CREATE TABLE IF NOT EXISTS Day
 (
 DayDate date NOT NULL PRIMARY KEY,
 StartTime time,
-EndTime time
+EndTime time,
+Status varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS Waitlist
