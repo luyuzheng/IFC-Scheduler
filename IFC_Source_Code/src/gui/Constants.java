fe15e751723d2d4d84776fe438b989b38d4ef88a
Valerie Roske <var34@cornell.edu>
2012-11-16 11:06:49 -0500

    added some comments

--- IFC_Source_Code/src/gui/Constants.java
+++ IFC_Source_Code/src/gui/Constants.java
@@ -1,5 +1,5 @@
 /**
- * Contains all the constants for the application
+ * Contains all the constants for the application.
  */
 
 package gui;
@@ -10,20 +10,27 @@ import java.awt.Font;
 
 
 public class Constants {
-	/**
-	 * The number of pixels that each minute corresponds to in the calendar area
-	 */
+	/** The number of pixels that each minute corresponds to in the calendar area */
 	public static final int PIXELS_PER_MINUTE = 4;
+	/** The height of the Heading Panel in the Day Panel. */
 	public static final int HEADING_PANEL_HEIGHT = 75;
+	/** The width of the Time Panel column in the Day Panel. */
 	public static final int TIMES_PANEL_WIDTH = 75;
+	/** The minimum width of an appointment block. */
 	public static final int MIN_APPT_PANEL_WIDTH = 300;
+	/** The minimum height of an appointment block. */
 	public static final int MIN_APPT_PANEL_HEIGHT = 300;
+	/** The width of the Appointment Subpanel. */
 	public static final int APPT_SUBPANEL_WIDTH = 200;
-	
-	public static final int DEFAULT_START_TIME = 960; 
+	/** The default start time for a day, given in minutes since 12AM. Default is 4PM. */
+	public static final int DEFAULT_START_TIME = 960;
+	/** The default closing time for a day, given in minutes since 12AM. Default is 8PM. */
 	public static final int DEFAULT_END_TIME = 1200;
+	/** The width of a day block in Month Panel. */
 	public static final int MONTH_BLOCK_WIDTH = 150;
+	/** The height of a day block in Month Panel. */
 	public static final int MONTH_BLOCK_HEIGHT = 100;
+	
 	
 	//public static final String PRAC_FILE_LOCATION = "data" + File.separator + "prac.txt";
 	//public static final String PAT_FILE_LOCATION = "data" + File.separator + "pat.txt";
@@ -32,9 +39,13 @@ public class Constants {
 	//public static final String WAITLIST_FILE_LOCATION = "data" + File.separator + "waitlist.txt";
 	//public static final String DEFAULT_FILE_LOCATION = "data" + File.separator + "defaults.txt";
 	
+	/** Default font for header styles */
 	public static final Font HEADER = new Font("Arial", Font.BOLD, 16);
+	/** Default font for paragraph styles */
 	public static final Font PARAGRAPH = new Font("Arial", Font.PLAIN, 14);
 	
+	/** The horizontal print margin, in pixels */
 	public static final int PRINT_MARGINX = 25;
+	/** The vertical print margin, in pixels */
 	public static final int PRINT_MARGINY = 25;
 }
--- IFC_Source_Code/src/gui/TimeSlot.java
+++ IFC_Source_Code/src/gui/TimeSlot.java
@@ -1,39 +1,47 @@
 /**
- * A time slot is two time objects corresponding to the start and end of the 
- * slot. 
+ * A time slot corresponds to the start and end time of an Appointment Block. 
  */
 
 package gui;
 
 public class TimeSlot {
+	/** The starting time of an appointment, in minutes since 12AM. */
 	private int startTime;
+	/** The ending time of an appointment, in minutes since 12AM. */
 	private int endTime;
 	
+	/** Constructs a time slot object given starting and ending times. */
 	public TimeSlot(int st, int et) {
 		startTime = st;
 		endTime = et;
 	}
 	
+	/** Returns the starting time of an appointment. */
 	public int getStartTime() {
 		return startTime;
 	}
 	
+	/** Returns the ending time of an appointment. */
 	public int getEndTime() {
 		return endTime;
 	}
 	
+	/** Returns the duration of an appointment. */
 	public int length() {
 		return endTime - startTime;
 	}
 	
+	/** Returns the number of hours given time in minutes. */
 	public int getHours(int time) {
 		return time/60;
 	}
 	
+	/** Returns the remainder number of minutes in an hour, given time in minutes. */
 	public int getMinutes(int time) {
 		return time%60;
 	}
 	
+	/** Displays the time slot as a string. */
 	public String toString() {
 		String string= "" + getHours(startTime) + ":" + getMinutes(startTime) + " - " + getHours(endTime) + ":" + getMinutes(endTime);
 		return string;
