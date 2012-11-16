4fde777aa8d38e51e1513b2e8b0ed1d155e40705
Claire Cipriani <cec247@cornell.edu>
2012-11-16 15:14:10 -0500

    Revert "Revert "Revert "fixed get all patients"""
    This reverts commit 23062362828b91d00e7da96cfb62523bbfff3302.

--- IFC_Source_Code/src/backend/DataService/DataServiceImpl.java
+++ IFC_Source_Code/src/backend/DataService/DataServiceImpl.java
@@ -255,10 +255,9 @@ public class DataServiceImpl implements DataService {
 
 		try {
 			st = connection.prepareStatement("Select Patient.PatID, Patient.FirstName, " +
-					"Patient.LastName, Patient.PhoneNumber, Patient.Notes, " +
-					"temp.NumberOfNoShows From Patient LEFT JOIN (Select PatID, " +
-					"Count(NoShowID) as NumberOfNoShows from NoShow Group by PatID) as " +
-					"temp ON temp.PatID = Patient.PatID");
+					"Patient.LastName, Patient.PhoneNumber, Patient.Notes, temp.NumberOfNoShows  " +
+					"From PatientLEFT JOIN (Select PatID, Count(NoShowID) as " +
+					"NumberOfNoShows from NoShowGroup by PatID) as temp ON temp.PatID = Patient.PatID");
 			rs = st.executeQuery();
 			List<PatientDto> results = new ArrayList<PatientDto>();
 			PatientDto patient = new PatientDto();
@@ -1493,18 +1492,10 @@ public class DataServiceImpl implements DataService {
     public ArrayList<AppointmentDto> searchForAppointments(TypeDto type) {
         PreparedStatement st = null;
         ResultSet rs = null;
-        ArrayList<AppointmentDto> availableAppointments = new ArrayList<AppointmentDto>();
         try {
-        	st = connection.prepareStatement("Select * FROM Appointment INNER JOIN " +
-        			"Practitioner ON Practitioner.PractID= Appointment.PractSchedID " +
-        			"WHERE PatID IS NULL AND ApptDate > NOW() AND Practitioner.`TypeID` = ?");
-        	st.setInt(1, type.getTypeID());
-        	rs = st.executeQuery();
-        	while (rs.next()){
-        		availableAppointments;
-        	}
-        	
-		     
+		st.execute();
+		//TODO: CLAIRE, list of query to get list of all appointments of type type (need to get type from PractSchedId->PractId->TypeID 
+              
                 
 	} catch (SQLException e) {
 		Logger lgr = Logger.getLogger(DataServiceImpl.class.getName());
