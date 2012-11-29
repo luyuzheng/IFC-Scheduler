0752436a849bfb39174b8c2520cef9e4275252d2
Jean Hooi Lee <jhl233@cornell.edu>
2012-11-28 21:39:31 -0500

    Added the listener that listens for mouse clicks and schedules a waitlist patient.

---
+++ IFC_Source_Code/src/gui/main/listeners/ScheduleWaitlistPatientListener.java
@@ -0,0 +1,37 @@
+package gui.main.listeners;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTable;
+
+import gui.sub.DisplayWaitingPatientUI;
+import gui.sub.DisplayWaitingPatientUI.*;
+
+import backend.DataService.DataServiceImpl;
+import backend.DataTransferObjects.AppointmentDto;
+
+public class ScheduleWaitlistPatientListener extends MouseAdapter {
+	
+	JTable owner;
+	Component parent;
+	
+	public ScheduleWaitlistPatientListener(JTable owner, Component parent) {
+		this.owner = owner;
+		this.parent = parent;
+	}
+	
+	/** Schedules a waitlisted patient to the day view. */
+	public void mouseClicked(MouseEvent e) {
+		//looking for double click events
+		if (e.getClickCount() >= 2) {
+			if (owner.getSelectedRow() >= 0) {
+				AppointmentDto appt = ((ApptTableModel)owner.getModel()).getAppointment(owner.getSelectedRow());
+				DataServiceImpl.GLOBAL_DATA_INSTANCE.addPatientToAppointment(
+					((DisplayWaitingPatientUI)parent).getWaitlistPatient().getPatID(), appt);
+				parent.setVisible(false);
+			}
+		}
+	}
+}
